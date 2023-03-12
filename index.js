ab3d0dbbb629974e2d67decbe2a5b8a1c3f04153
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-12 10:11:57 +0300

    homepage and css file

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-let hover = document.getElementById('p2');
-
-let hiddenDiv = document.getElementById('hide');
-
-hover.addEventListener('mouseover',function handleMouseOver(){
-    hiddenDiv.style.visibility= 'visible';
-});
-
-hover.addEventListener('mouseout',function handleMouseOut(){
-    hiddenDiv.style.visibility= 'hidden';
-});
+function myFunction() {
+    var x = document.getElementById("hide");
+    if (x.style.display == "bock") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
